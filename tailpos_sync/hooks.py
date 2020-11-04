776f1ad7767ddb1c428885910d90e344264c2388
Santosh Baburao <santosh.baburao@gmail.com>
2020-11-04 19:28:07 +0000

    hooks.py edited

--- tailpos_sync/hooks.py
+++ tailpos_sync/hooks.py
@@ -4,63 +4,22 @@ from . import __version__ as app_version
 
 app_name = "tailpos_sync"
 app_title = "TailPOS Sync"
-app_publisher = "Bai Web and Mobile Lab"
+app_publisher = "Taazur Limited Company"
 app_description = "TailPOS ERPNext Sync"
 app_icon = "octicon octicon-file-directory"
 app_color = "#3498db"
-app_email = "hello@bai.ph"
+app_email = "santosh.baburao@gmail.com"
 app_license = "MIT"
 
 fixtures = [
-    {
-        "doctype": "Custom Field",
-        "filters": [
-            [
-                "name",
-                "in",
-                [
-                    "Customer-note",
-                    "Customer-phonenumber",
-                    "Customer-email",
-                    "Customer-id",
-                    "Customer-date_updated",
-                    "Deleted Document-sync_status",
-                    "Item-tail_sb",
-                    "Item-tail_cb",
-                    "Item-date_updated",
-                    "Item-id",
-                    "Item-favorite",
-                    "Item-barcode",
-                    "Item-in_tailpos",
-                    "Item-sku",
-                    "Item-color",
-                    "Item-shape",
-                    "Item-category",
-                    "Item-color_or_image",
-                    "Payment Entry-wallet_card_number",
-                    "Payment Entry-top_up_wallet",
-                    "Customer Credit Limit-total_prepaid_balance",
-                    "Error Log-device_id",
-                    "Error Log-wallet_card",
-                    "Error Log-pin",
-                    "Item-arabic_text",
-                    "Sales Invoice-receipt",
-                    "Item Tax Template Detail-tax_translation",
-                    "Loyalty Program-default"
-                ]
-            ]
-        ]
-    },
+
     "Custom Script",
     "Property Setter",
     "Print Format",
     "Report",
-    "Role",
-    "DocType",
     "Letter Head",
     "Workflow",
     "Workflow State",
-    "Workflow Action",
     "Custom Field"
 ]
 
@@ -79,11 +38,13 @@ fixtures = [
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {
-    "Payment Entry" : "public/js/payment_entry.js",
-    "Customer" : "public/js/customer.js",
-    "Sales Invoice" : "public/js/sales_invoice.js",
-}
+#----------------------------------------------------------------------------------------
+#doctype_js = {
+#    "Payment Entry" : "public/js/payment_entry.js",
+#    "Customer" : "public/js/customer.js",
+#    "Sales Invoice" : "public/js/sales_invoice.js",
+#}
+#---------------------------------------------------------------------------------------
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -149,19 +110,20 @@ doc_events = {
     "Payment Entry": {
         "on_submit": "tailpos_sync.doc_events.payment_entry.on_submit",
     },
-    "Loyalty Program": {
-        "validate": "tailpos_sync.doc_events.loyalty_program.update_loyalty_programs",
-    },
+#    "Loyalty Program": {
+#        "validate": "tailpos_sync.doc_events.loyalty_program.update_loyalty_programs",
+#    },
 
 }
 
 # Scheduled Tasks
 # ---------------
 
-scheduler_events = {
-    "hourly": [
-        "tailpos_sync.background_jobs.generate_si"
-    ]
+#---------------------------------------------------------------------------------------------------
+#scheduler_events = {
+#    "hourly": [
+#        "tailpos_sync.background_jobs.generate_si"
+#    ]
     # "* * * * *": [
     #     "tailpos_erpnext.tasks.sync_couchdb"
     # ],
@@ -180,8 +142,8 @@ scheduler_events = {
     # "monthly": [
     # 	"tailpos_erpnext.tasks.monthly"
     # ]
-}
-
+#}
+#----------------------------------------------------------------------------------------------------
 # Testing
 # -------
 
